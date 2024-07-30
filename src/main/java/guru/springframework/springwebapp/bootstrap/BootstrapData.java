23799861ffd0407f32b83ffaebeb72cc33b78f7f
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-07-30 23:47:41 +0200

    H2 console enable and correct bugs add

--- src/main/java/guru/springframework/springwebapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/springwebapp/bootstrap/BootstrapData.java
@@ -46,6 +46,8 @@ public class BootstrapData implements CommandLineRunner{
 
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
+        dddSaved.getAuthors().add(ericSaved);
+        noEJBSaved.getAuthors().add(rodSaved);
         Publisher publisher = new Publisher();
         publisher.setPublisherName("SFG Publishing");
         publisher.setCity("St Petersburg");
