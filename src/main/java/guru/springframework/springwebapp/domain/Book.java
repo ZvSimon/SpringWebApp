8f06589ff81e44e637969a9aa59a379c1cfd6fb6
Simon <73712759+ZvSimon@users.noreply.github.com>
2024-07-30 19:16:59 +0200

    JPA RelationShips between Author and Book

--- src/main/java/guru/springframework/springwebapp/domain/Book.java
+++ src/main/java/guru/springframework/springwebapp/domain/Book.java
@@ -1,9 +1,6 @@
 package guru.springframework.springwebapp.domain;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Book {
@@ -12,9 +9,29 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
 
     public String getTitle() {
         return title;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setTitle(String title) {
